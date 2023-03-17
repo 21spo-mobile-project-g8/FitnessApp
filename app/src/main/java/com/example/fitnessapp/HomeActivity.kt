4b69bdc96db3b3d87ffa4225dc3d836a3a40cbaa
arttukaariainen <t1kaar01@students.oamk.fi>
2023-03-17 20:37:40 +0200

    Dark mode valmis, activityihin siirtyminen ja ulkoasun muokkaamista

--- app/src/main/java/com/example/fitnessapp/HomeActivity.kt
+++ app/src/main/java/com/example/fitnessapp/HomeActivity.kt
@@ -1,11 +1,63 @@
 package com.example.fitnessapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        var settingsButton : Button = findViewById(R.id.settingsButton)
+        settingsButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java).apply{
+
+            }
+            startActivity(intent);
+        }
+
+        var foodButton : Button = findViewById(R.id.foodButton)
+        foodButton.setOnClickListener {
+            val intent = Intent(this, FoodActivity::class.java).apply{
+
+            }
+            startActivity(intent);
+        }
+
+        var exerciseButton : Button = findViewById(R.id.exerciseButton)
+        exerciseButton.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java).apply{
+
+            }
+            startActivity(intent);
+        }
+
+        var caloriesButton : Button = findViewById(R.id.caloriesButton)
+        caloriesButton.setOnClickListener {
+            val intent = Intent(this, CaloriesActivity::class.java).apply{
+
+            }
+            startActivity(intent);
+        }
+
+        var stepsButton : Button = findViewById(R.id.stepsButton)
+        stepsButton.setOnClickListener {
+            val intent = Intent(this, StepsActivity::class.java).apply{
+
+            }
+            startActivity(intent);
+        }
+
+        var trackerButton : Button = findViewById(R.id.trackerButton)
+        trackerButton.setOnClickListener {
+            val intent = Intent(this, TrackerActivity::class.java).apply{
+
+            }
+            startActivity(intent);
+        }
     }
 }
+
