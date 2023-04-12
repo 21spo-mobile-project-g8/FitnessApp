88a30b818082063e2c9468e72e1bf8014d7ff0a7
Juhani Koski <juhkukoskii@gmail.com>
2023-04-12 10:12:44 +0300

    menu aloitus

--- app/src/main/java/com/example/fitnessapp/api/ApiInterface.kt
+++ app/src/main/java/com/example/fitnessapp/api/ApiInterface.kt
@@ -3,9 +3,10 @@ package com.example.fitnessapp.api
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Query
 
 interface ApiInterface {
     @Headers("X-Api-Key: " + "W3QAJsTRC9vBQ8Q4PGTi7wKEA4gQNHSOLcCmfDvu")
-    @GET("exercises?muscle=lats")
-    fun getData(): Call<DataModel>
+    @GET("exercises")
+    fun getData(@Query("muscle")muscle: String): Call<DataModel>
 }
